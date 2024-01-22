758b85a95b9c21e07d8f5d7a511aa969bf8fa174
Robert Portelli <github@robertportelli.com>
2024-01-22 10:46:08 -0500

    fixes #29

--- src/quecital/add_recital.py
+++ src/quecital/add_recital.py
@@ -3,11 +3,12 @@
 
 from tomli_w import dump
 from quecital.quiz import main_loop
+from quecital.multiline_text_editor import get_multiline_edit
 
 
 def main(quecital_toml_path, quecital_data):
     questions_list = path_to_list_of_dicts(quecital_data)
-    new_question_dict = create_recital()
+    new_question_dict = form_toml_entry()
     questions_list.append(new_question_dict)
     with open(quecital_toml_path, "wb") as f:
         dump(quecital_data, f)
@@ -31,27 +32,14 @@ def path_to_list_of_dicts(quecital_data):
 #  TODO handle the key error if ["recitals"] not in quecital_data[topic_label]
 
 
-def create_recital():
-    """
-    Create a dictionary representing a new trivia question.
-
-    Returns:
-        Dict: A dictionary with the following items:
-            - 'question': str,
-            - 'answers': List,
-            - 'alternatives': List,
-            - 'hint': None,
-            - 'explanation': None
-    """
-    prompt = input("Enter your prompt: ")
-    recital = input("Enter your recital: ")
-    alternatives = input("Enter one alternative at a time: ")  # TODO a string to be parsed
-    hint = input("Offer a hint? -enter to skip")
-    explanation = input("Offer an explanation? - enter to skip")
+def form_toml_entry():
+    prompt = get_multiline_edit("Enter your prompt: ")
+    recital = get_multiline_edit("Enter your recital: ")
+    hint = get_multiline_edit("Offer a hint? -enter to skip")
+    explanation = get_multiline_edit("Offer an explanation? - enter to skip")
     return {
-        "prompt": prompt,
-        "recital": list([recital]),
-        "alternatives": list([alternatives]),
+        "prompt": prompt,  # the "question"
+        "recital": recital,  # the "answer"
         "hint": hint,
         "explanation": explanation,
     }
