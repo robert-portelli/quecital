1170bcd63d3a4735530c83ac2ee6847c1bd4ea97
Robert Portelli <github@robertportelli.com>
2024-01-24 09:18:23 -0500

    refactor

--- src/quecital/recital.py
+++ src/quecital/recital.py
@@ -1,3 +1,8 @@
+# recital.py
+"""
+This module provides functions for conducting random recitals
+and displaying recital details.
+"""
 import click
 from pathlib import Path
 import tomllib
@@ -6,6 +11,15 @@ from quecital.multiline_text_editor import get_multiline_edit
 
 
 def main(data):
+    """
+    Conduct a random recital and display details including the prompt, attempt, and correct answer.
+
+    Args:
+        data (dict): A dictionary containing recital data.
+
+    Returns:
+        None
+    """
     assets = preprocess(data)
     attempt = get_attempt(assets)
     display = f"\
@@ -19,20 +33,41 @@ def main(data):
         display += f"\n\nExplanation: {assets['explanation']}"
     click.echo(display)
 
+
 def compose_prompt(assets, show_hint=False):
+    """
+    Compose the prompt for a recital, optionally including a hint.
+
+    Args:
+        assets (dict): A dictionary containing recital assets.
+        show_hint (bool, optional): A flag indicating whether to include the hint. Defaults to False.
+
+    Returns:
+        str: The composed prompt.
+    """
     hint = "Hint not provided"
     if "hint" in assets and assets["hint"] and show_hint is False:
         hint = "<DELETE FOR HINT>"
     if "hint" in assets and assets["hint"] and show_hint is True:
-        hint = assets['hint']
+        hint = assets["hint"]
     prompt = f'The prompt is:\n\n{assets["question"]}\n\n{hint}'
     return prompt
 
 
 def get_attempt(assets):
+    """
+    Get the user's attempt for a recital, handling the option to
+    view the hint.
+
+    Args:
+        assets (dict): A dictionary containing recital assets.
+
+    Returns:
+        str: The user's attempt.
+    """
     prompt = compose_prompt(assets)
     attempt = get_multiline_edit(prompt)
-    if attempt == 'show_hint':
+    if attempt == "show_hint":
         prompt = compose_prompt(assets, show_hint=True)
         attempt = get_multiline_edit(prompt, hint_shown=True)
 
@@ -40,18 +75,27 @@ def get_attempt(assets):
 
 
 def preprocess(quecital_data):
+    """
+    Preprocess quecital data and prompt the user to choose a topic for a random recital.
+
+    Args:
+        quecital_data (dict): A dictionary containing quecital data.
+
+    Returns:
+        dict: Recital assets for the chosen topic.
+    """
     topics = list(quecital_data.keys())
     prompt = "\nFrom which topic will you perform a random recital\n\n"
     for index, topic in enumerate(topics, start=1):
         prompt += f"\n{index}: {topic}"
-    prompt += '\n\nEnter one of the following ->'
+    prompt += "\n\nEnter one of the following ->"
     valid_choices = list(map(str, range(1, len(topics) + 1)))
     selected_index = click.prompt(prompt, type=click.Choice(valid_choices))
     topic = topics[int(selected_index) - 1]
     return random.choice(quecital_data[topic]["recitals"])
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     quecital_path = Path("quecital.toml")
     data = tomllib.loads(quecital_path.read_text(encoding="utf-8"))
     main(data)
